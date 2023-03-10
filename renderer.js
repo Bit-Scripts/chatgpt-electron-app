7dc66bf45af471221db5d6db3752418ae3b7fcbb
Paul WOISARD <paulwoisard@gmail.com>
2023-03-10 06:48:21 +0100

    coloration syntaxique

--- renderer.js
+++ renderer.js
@@ -56,6 +56,21 @@ if (titreApp !== null) {
 
 
 function exchangeMessages() {
+    marked.setOptions({
+        renderer: new marked.Renderer(),
+        highlight: function(code, lang) {
+            const hljs = require('highlight.js');
+            const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+            return hljs.highlight(code, { language }).value;
+        },
+        langPrefix: 'hljs language-', // highlight.js css expects a top-level 'hljs' class.
+        pedantic: false,
+        gfm: true,
+        breaks: false,
+        sanitize: false,
+        smartypants: false,
+        xhtml: false
+    });
     let first = true
     let messages = document.getElementById("messages");
     let question = document.getElementById("question").value;
